c5046e691ef34cae83f42fa8c4915596414c8a3b
Ehsan Mirtalebi <ehsan.mirtalebi@gmail.com>
2025-07-25 14:31:18 +0330

    create Site Settings table and model

---
+++ app/Models/SiteSetting.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SiteSetting extends Model
+{
+    use HasFactory;
+
+    const APP_NAME = "APP_NAME";
+
+
+    const defualtValues = [
+        self::APP_NAME => "قاطی پلو",
+    ];
+
+    public static function getValue($name)
+    {
+        $ss = SiteSetting::where('name', $name)->first();
+        if ($ss) {
+            return $ss->value;
+        }
+
+        return self::defualtValues[$name];
+    }
+
+    public static function setValue($name, $value)
+    {
+        $ss = SiteSetting::where('name', $name)->first();
+        if ($ss) {
+            $ss->value = $value;
+            $ss->save();
+        }
+
+        return $ss;
+    }
+}
